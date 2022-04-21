b2d4c36b7df94aa0a7173087d2a04f7299f645e9
erotourtes <erotourtes@gmail.com>
2022-04-21 19:22:22 +0300

    first page

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log($("p").text());
+anime({
+    targets: ".moveBtns h2",
+    translateY: [-50, 0],
+    delay: anime.stagger(100, {start: 250, from: "center"})
+});
+
+anime({
+    targets: [".mainInfo h1", ".form__group input"],
+    scale: [0, 1],
+});
+
+anime({
+    targets: ".footer",
+    translateY: [100, 0],
+    easing: "easeOutExpo"
+});
+
+anime({
+    targets: ".contactInfo p",
+    translateX: 30,
+    delay: anime.stagger(100, {start: 250})
+});
+
+anime({
+    targets: ".icons svg",
+    translateY: [0, 1, -1, 0],
+    delay: anime.stagger(100),
+    easing: "easeOutExpo",
+    loop: true
+});
+
+
+
+$(document).ready(()=>{
+    $(".moveBtns h2").hover(function(event){
+        $(this).addClass("rotate");
+    });
+    
+});
